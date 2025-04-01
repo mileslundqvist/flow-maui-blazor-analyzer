b709d62177bacb70bd4afab5454dad1a9a048caf
Miles Lundqvist <mileslundqvist@gmail.com>
2025-04-01 15:01:16 +0200

    Add test project and initial test class, not implemented yet.

---
+++ MauiBlazorAnalyzer.Tests/JsInvokableMethodAnalyzerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MauiBlazorAnalyzer.Tests;
+
+public class JsInvokableMethodAnalyzerTests
+{
+    [Fact]
+    public async Task EmptyCode_ShouldNotReportDiagnostic()
+    {
+        var testCode = @"";
+        int a = 3;
+        Assert.True(a == 3);
+
+    }
+}
