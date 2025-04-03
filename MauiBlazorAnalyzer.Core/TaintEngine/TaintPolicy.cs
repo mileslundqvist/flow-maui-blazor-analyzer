ffbcf4d811162d7234e072c339dc178bd15f2493
Miles Lundqvist <mileslundqvist@gmail.com>
2025-04-03 08:14:02 +0200

    Added beginning of taint analysis framework with state management (very initial overview)

---
+++ MauiBlazorAnalyzer.Core/TaintEngine/TaintPolicy.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBlazorAnalyzer.Core.TaintEngine;
+
+public static class TaintPolicy
+{
+    public static bool IsSource(IMethodSymbol methodSymbol)
+    {
+        // TODO: Implement a more comprehensive check for sources
+        return methodSymbol.ToString().Contains("System.Console.Readline");
+    }
+
+    public static bool IsSink(IInvocationOperation invocation, int argumentIndex, AnalysisState currentState)
+    {
+        // TODO: Implement a more comprehensive check for sinks
+        if (invocation.TargetMethod.ToString().Contains("SqlCommand.CommandText.set") && argumentIndex == 0)
+        {
+            var argument = invocation.Arguments[argumentIndex].Value;
+
+            if (argument is ILocalReferenceOperation localRef && currentState.GetTaint(localRef.Local) == TaintState.Tainted) return true;
+            if (argument is IParameterReferenceOperation paramRef && currentState.GetTaint(paramRef.Parameter) == TaintState.Tainted) return true;
+        }
+        return false;
+    }
+
+    public static bool IsSanitizer(IInvocationOperation invocation)
+    {
+        // TODO: Implement a more comprehensive check for sanitizers
+        return invocation.TargetMethod.ToString().Contains("MySanitizer.Sanitize");
+    }
+}
